6f1c98c08c74d57698ce2fea1070f22bd48f59b6
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2026-01-29 12:42:08 +0700

    update folder assets

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('unify.intro');
 
 // Include Unify routes
 require __DIR__ . '/main.php';
